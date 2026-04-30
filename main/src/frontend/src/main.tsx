20047523d3b8ef89947c28276c4a1c25c4913aa4
Chaitanya Patankar <easy_internet_income@yahoo.com>
2026-04-30 23:04:32 +0530

    final service worker setup

--- main/src/frontend/src/main.tsx
+++ main/src/frontend/src/main.tsx
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+// Fix BigInt serialization
 BigInt.prototype.toJSON = function () {
   return this.toString();
 };
@@ -16,6 +17,7 @@ declare global {
 
 const queryClient = new QueryClient();
 
+// Render React App
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
     <InternetIdentityProvider>
@@ -23,3 +25,28 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     </InternetIdentityProvider>
   </QueryClientProvider>,
 );
+
+// Register Service Worker (FINAL VERSION)
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/sw.js")
+    .then((reg) => {
+      console.log("Service Worker registered", reg);
+
+      reg.onupdatefound = () => {
+        const installingWorker = reg.installing;
+        if (installingWorker) {
+          installingWorker.onstatechange = () => {
+            if (installingWorker.state === "installed") {
+              if (navigator.serviceWorker.controller) {
+                console.log("New update available");
+              } else {
+                console.log("Content cached for offline use");
+              }
+            }
+          };
+        }
+      };
+    })
+    .catch((err) => console.log("Service Worker failed", err));
+}
