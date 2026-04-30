be685e8a623bd2ba416b0687390ad22ae00e70c6
Chaitanya Patankar <easy_internet_income@yahoo.com>
2026-05-01 00:52:39 +0530

    fix manifest errors for pwa

--- main/src/frontend/public/sw.js
+++ main/src/frontend/public/sw.js
@@ -14,6 +14,22 @@ self.addEventListener("install", (event) => {
             return cache.addAll(urlsToCache);
         })
     );
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.map((key) => {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
